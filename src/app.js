68278ff120992055630911848f97159d4442f476
Salmanulfaris-M <fsalmanul13@gmail.com>
2025-05-23 15:34:32 +0530

    added patch conditon allow in corsoptions

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 // CORS Configuration
 const corsOptions = {
   origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST","Patch", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
